7d2079daca5299663422d7d18a4c5338b9f6bc99
Shivam Garg <sgdpsi@gmail.com>
2021-10-21 17:50:46 -0600

    minor

---
+++ aistats_code_submit/environments.py
@@ -0,0 +1,162 @@
+import numpy as np
+import pdb
+
+#======================================================================
+# Saving the environment data from OLD's environment
+#----------------------------------------------------------------------
+# from src.envs.gridworld_mdp import cliff_gw
+# env = cliff_gw(gamma=0.99)
+# np.savez('cliff_world_env', P=env.P, r=env.R, mu=env.p0,
+#          terminal_states=env.terminal_states)
+# arr_dict = np.load('cliff_world_env.npz')
+# P_OLD = arr_dict['P']
+# r_OLD = arr_dict['r']
+# mu_OLD = arr_dict['mu']
+# terminal_states_OLD = [48]
+#======================================================================
+
+#======================================================================
+# Simplified CliffWorld (re-write)
+#----------------------------------------------------------------------
+# -------------------         4 is the goal state
+# | 4 | 9 | 14 | 19 |         20 is terminal state reached only via the state 4
+# -------------------         3, 2, 1 are chasms  
+# | 3 | 8 | 13 | 18 |         0 is the start state
+# ------------------- 
+# | 2 | 7 | 12 | 17 |         all transitions are determinitic
+# -------------------         Actions: 0=down, 1=up, 2=left, 3=right
+# | 1 | 6 | 11 | 16 |
+# ------------------------    rewards are all zeros except at chasms (-100)
+# | 0 | 5 | 10 | 15 | 20 |    reward for going into the goal state is +1
+# ------------------------
+#----------------------------------------------------------------------
+
+# environment P: (i, j, k)th element = Pr{S' = s_k | S = s_i, A = a_j}
+P = np.zeros((21, 4, 21))
+for state_idx in range(21):
+    for action_idx in range(4):
+        if state_idx in [1, 2, 3]: # chasms: reset to start state 0
+            new_state_idx = 0
+        elif state_idx == 4: # goal state: agent always goes to 20
+            new_state_idx = 20
+        elif state_idx == 20: # terminal state
+            new_state_idx = 20
+        else: # move according to the deterministic dynamics
+            x_new = x_old = state_idx // 5
+            y_new = y_old = state_idx % 5
+            if action_idx == 0: # Down
+                y_new = np.clip(y_old - 1, 0, 4)
+            elif action_idx == 1: # Up
+                y_new = np.clip(y_old + 1, 0, 4)
+            elif action_idx == 2: # Left
+                x_new = np.clip(x_old - 1, 0, 3)
+            elif action_idx == 3: # Right
+                x_new = np.clip(x_old + 1, 0, 3)
+            new_state_idx = 5 * x_new + y_new
+
+        P[state_idx, action_idx, new_state_idx] = 1
+        
+r = np.zeros((21, 4))
+r[1, :] = r[2, :] = r[3, :] = -100 # negative reward for falling into chasms
+r[4, :] = +1 # positive reward for finding the goal terminal state
+
+mu = np.zeros(21)
+mu[0] = 1
+
+terminal_states = [20]
+#======================================================================
+        
+class CliffWorld():
+    def __init__(self, P, r, mu, terminal_states, gamma, episode_cutoff_length,
+                 reward_noise):
+        self.P = P
+        self.r = r
+        self.mu = mu
+        self.terminal_states = terminal_states
+        
+        self.state_space = self.r.shape[0]
+        self.action_space = self.r.shape[1]
+
+        self.gamma = gamma
+        self.episode_cutoff_length = episode_cutoff_length
+        self.reward_noise = reward_noise
+
+        self.t = None
+        self.state = None
+
+    def reset(self):
+        self.t = 0
+        self.state = np.random.choice(a=self.state_space, p=self.mu)
+        return self.state
+
+    def step(self, action):
+        if self.state is None:
+            raise Exception('step() used before calling reset()')
+        assert action in range(self.action_space)
+
+        reward = self.r[self.state, action] \
+            + np.random.normal(loc=0, scale=self.reward_noise)
+        self.state = np.random.choice(a=self.state_space,
+                                      p=self.P[self.state, action])
+        self.t = self.t + 1
+
+        done = 'false'
+        if self.state in self.terminal_states:
+            done = 'terminal'
+        elif self.t > self.episode_cutoff_length:
+            done = 'cutoff'
+
+        return self.state, reward, done
+
+    # for computing v^pi = (I - gamma P_pi)^{-1} r_pi
+    # environment P: (i, j, k)th element = Pr{S' = s_k | S = s_i, A = a_j}
+    # policy pi: (i, j)th element = Pr{A = a_j | S = s_i}
+    # P_pi(s' | s) = sum_a P(s' | s, a) * pi(a | s)
+    def calc_vpi(self, pi, FLAG_RETURN_V_S0=False):
+        p_pi = np.einsum('xay,xa->xy', self.P, pi)
+        r_pi = np.einsum('xa,xa->x', self.r, pi)
+        v_pi = np.linalg.solve(np.eye(self.state_space) - self.gamma * p_pi,
+                               r_pi)
+        if FLAG_RETURN_V_S0: # calculate v_s0
+            v_s0 = np.dot(self.mu, v_pi)
+            return v_s0
+        else:
+            return v_pi
+
+    # compute q^pi = r(s, a) + gamma * sum_s' p(s' | s, a) * v^pi(s')
+    def calc_qpi(self, pi):
+        v_pi = self.calc_vpi(pi)
+        q_pi = self.r + self.gamma * np.einsum('xay,y->xa', self.P, v_pi)
+        return q_pi
+
+    # computing the normalized occupancy measure
+    # d^pi = (1 - gamma) * mu (I - gamma P_pi)^{-1};   mu = start state dist
+    def calc_dpi(self, pi):
+        p_pi = np.einsum('xay,xa->xy', self.P, pi)
+        d_pi = (1 - self.gamma) * \
+            np.linalg.solve(np.eye(self.state_space) - self.gamma * p_pi.T,
+                            self.mu)
+        d_pi /= d_pi.sum() # for addressing numerical errors
+        return d_pi
+
+    def calc_q_star(self, num_iters=1000):
+        q = np.zeros((self.state_space, self.action_space))
+        for i in range(num_iters):
+            q_new = self.r + np.einsum("xay,y->xa",
+                                       self.gamma * self.P, q.max(1))
+            q = q_new.copy()
+        return q
+
+    def calc_v_star(self, num_iters=1000):
+        v = np.zeros(self.state_space)
+        for i in range(num_iters):
+            v_new = self.r + np.einsum("xay,y->xa", self.gamma * self.P, v)
+            v_new = v_new.max(1)
+            v = v_new.copy()
+        return v
+
+    def calc_pi_star(self, num_iters=1000): # just go greedy wrt q_star
+        q_star = self.calc_q_star(num_iters=num_iters)
+        pi_star = np.zeros((self.state_space, self.action_space))
+        pi_star[range(self.state_space), q_star.argmax(1)] = 1
+        return pi_star
---
+++ aistats_code_submit/run_exp.py
@@ -0,0 +1,99 @@
+import pdb
+import time
+import argparse
+import os
+import json
+
+from environments import *
+from utils import *
+
+# $ python run_exp.py --num_outer_loops 2000 --num_inner_loops 1000 --pg_alg 'TRPO' --eta -1 --alpha -1 --epsilon -1 --delta 0.0001 --decay_factor 0.9 --use_analytical_grad 0
+
+# read command line arguments to get parameter configurations
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--num_outer_loops', required=True, type=int)
+parser.add_argument('-m', '--num_inner_loops', required=True, type=int)
+parser.add_argument('-alg', '--pg_alg', required=True, type=str)
+parser.add_argument('-e', '--eta', required=True, type=float)
+parser.add_argument('-a', '--alpha', required=True, type=float)
+parser.add_argument('-eps', '--epsilon', required=True, type=float)
+parser.add_argument('-d', '--delta', required=True, type=float)
+parser.add_argument('-dec', '--decay_factor', required=True, type=float)
+parser.add_argument('-tg', '--use_analytical_grad', required=True, type=int)
+parser.add_argument('-z', '--zeta', required=True, type=float)
+
+args = parser.parse_args()
+
+pg_method = args.pg_alg
+num_outer_loops = args.num_outer_loops
+num_inner_loops = args.num_inner_loops if args.num_inner_loops >= 0 else None
+eta = args.eta if args.eta >= 0 else None
+alpha = args.alpha if args.alpha >= 0 else None
+epsilon = args.epsilon if args.epsilon >= 0 else None
+delta = args.delta if args.delta >= 0 else None
+decay_factor = args.decay_factor if args.decay_factor >= 0 else None
+FLAG_ANALYTICAL_GRADIENT = False if args.use_analytical_grad == 0 else True
+zeta = args.zeta if args.zeta >= 0 else None
+
+FLAG_TRUE_ADVANTAGE = True
+num_traj_estimate_adv = None 
+adv_estimate_alg = None
+adv_estimate_stepsize = None
+FLAG_SAVE_INNER_STEPS = False
+
+# folder details
+folder_name = 'fmaPG_exp/CliffWorld_{}'.format(pg_method)
+os.makedirs(folder_name, exist_ok='True')
+
+#----------------------------------------------------------------------
+# create the environment
+#----------------------------------------------------------------------
+gamma = 0.9
+episode_cutoff_length = 1000
+reward_noise = 0
+
+env = CliffWorld(P=P, r=r, mu=mu, terminal_states=terminal_states,
+                 gamma=gamma, episode_cutoff_length=episode_cutoff_length,
+                 reward_noise=reward_noise)
+
+#----------------------------------------------------------------------
+# learning curves against the number of outer loop iterations
+#----------------------------------------------------------------------
+tic = time.time()
+vpi_list_outer, vpi_list_inner, pi = run_experiment(
+    env=env, pg_method=pg_method, num_iters=num_outer_loops,
+    eta=eta, delta=delta, decay_factor=decay_factor, epsilon=epsilon,
+    FLAG_ANALYTICAL_GRADIENT=FLAG_ANALYTICAL_GRADIENT,
+    num_inner_updates=num_inner_loops, alpha=alpha,
+    FLAG_TRUE_ADVANTAGE=FLAG_TRUE_ADVANTAGE,
+    adv_estimate_alg=adv_estimate_alg,
+    num_traj_estimate_adv=num_traj_estimate_adv,
+    adv_estimate_stepsize=adv_estimate_stepsize,
+    FLAG_SAVE_INNER_STEPS=FLAG_SAVE_INNER_STEPS, zeta=zeta)
+print('Total time taken: {}'.format(time.time() - tic))
+
+dat = dict()
+dat['vpi_outer_list'] = vpi_list_outer.tolist()
+if vpi_list_inner is not None:
+    vpi_list_inner = vpi_list_inner.tolist()
+dat['vpi_inner_list'] = vpi_list_inner
+dat['pi'] = pi.tolist()
+
+filename='{}/numOuterLoops_{}__numInnerLoops_{}__eta_{}__alpha_{}'\
+    '__epsilon_{}__delta_{}__decayFactor_{}__analyticalGrad_{}__zeta_{}'.format(
+        folder_name, num_outer_loops, num_inner_loops, eta, alpha, epsilon,
+        delta, decay_factor, FLAG_ANALYTICAL_GRADIENT, zeta)
+with open(filename, 'w') as fp:
+    json.dump(dat, fp)
+    
+# fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+# plot_grid(axs[0])
+# plot_policy(axs[0], pi)
+# axs[1].plot(vpi_list_outer)
+# v_star = np.dot(env.calc_v_star(), env.mu)
+# axs[1].set_ylim([-1, v_star + 0.05])
+# plt.savefig('lc_{}_{}_env.pdf'.format(pg_method, eta))
+# plt.show()  
+# plt.close()
+
+
